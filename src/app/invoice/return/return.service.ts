673172f291127c3398afd34513889b035789c808
kbebel97 <63519535+kbebel97@users.noreply.github.com>
2020-12-07 19:55:18 -0500

    Update return.service.ts

--- src/app/invoice/return/return.service.ts
+++ src/app/invoice/return/return.service.ts
@@ -1,43 +1,43 @@
-import { Injectable } from "@angular/core";
-import { invoice } from 'src/app/shared/invoice.model';
-import { purchasedItem } from 'src/app/shared/purchasedItem.model';
+// import { Injectable } from "@angular/core";
+// import { invoice } from 'src/app/shared/invoice.model';
+// import { purchasedItem } from 'src/app/shared/purchasedItem.model';
 
-@Injectable()
-export class returnService{
-  invoice: invoice;
-  purchasedItem: purchasedItem;
-  componentSelected: string;
-  height : number;
+// @Injectable()
+// export class returnService{
+//   invoice: invoice;
+//   purchasedItem: purchasedItem;
+//   componentSelected: string;
+//   height : number;
 
-  setInvoice(invoice: invoice){
-    this.invoice = invoice;
+//   setInvoice(invoice: invoice){
+//     this.invoice = invoice;
 
-  }
+//   }
 
-  setItem(purchasedItem: purchasedItem, invoice: invoice){
-    this.purchasedItem = purchasedItem;
-    this.invoice = invoice;
-  }
+//   setItem(purchasedItem: purchasedItem, invoice: invoice){
+//     this.purchasedItem = purchasedItem;
+//     this.invoice = invoice;
+//   }
 
-  setHeight(height : number){
-    this.height = height;
-    console.log(this.height);
-  }
+//   setHeight(height : number){
+//     this.height = height;
+//     console.log(this.height);
+//   }
 
-  getheight(){
-    return this.height;
-  }
+//   getheight(){
+//     return this.height;
+//   }
 
-  getInvoice(){
-    return this.invoice;
-  }
+//   getInvoice(){
+//     return this.invoice;
+//   }
 
-  getComponentSelected(){
-    return this.componentSelected;
-  }
+//   getComponentSelected(){
+//     return this.componentSelected;
+//   }
 
-  getPurchasedItem(){
-    return this.purchasedItem;
-  }
+//   getPurchasedItem(){
+//     return this.purchasedItem;
+//   }
 
-}
+// }
